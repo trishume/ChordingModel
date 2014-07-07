1358b95ead53bb350aa605853b193c81d0acc3db
Tristan Hume <tris.hume@gmail.com>
2014-07-07 16:38:18 -0400

    Finger model

--- model.rb
+++ model.rb
@@ -1,9 +1,20 @@
 # Model of VeloType typing
 require "state_machine"
 
+def load_layout
+  map = {}
+  File.foreach("layout.txt") do |line|
+    next if line.strip.empty? || line[0] == '#'
+    parts = line.chomp.split
+    map[parts[0]] = parts[1..-1].map {|k| k[0].to_i}
+  end
+  map
+end
+
+LAYOUT = load_layout
+
 class StrokeModel
-  LAYOUT = %w{zfs ptc kjr ioeua lny}
-  attr_accessor :parts
+  attr_accessor :parts, :pressed
   class CanNotStrokeError < StandardError; end
 
   state_machine :state, :initial => :begin do
@@ -42,10 +53,32 @@ class StrokeModel
   end
 
   def initialize
-    @pressed = {}
+    # 2 hands with 5 fingers + palm
+    @pressed = Array.new(2) { Array.new(6,false) }
     @state = :start
 
     @parts = [""]
+  end
+
+  def press(layer, finger)
+    return false if @pressed[layer][finger]
+    @pressed[layer][finger] = true
+    true
+  end
+
+  def alloc_vowel(c)
+    finger = LAYOUT[c][0]
+    if !press(0, finger)
+      return press(1, finger)
+    end
+    true
+  end
+
+  def alloc_finger(c)
+    return alloc_vowel(c) if self.middle?
+    fingers = LAYOUT[c]
+    layer = (self.final?) ? 1 : 0
+    return fingers.all? {|finger| press(layer,finger)}
   end
 
   def add(c)
@@ -57,33 +90,69 @@ class StrokeModel
     else
       self.special
     end
+    can_press = alloc_finger(c)
+    return :nofinger unless can_press
     @parts.last << c
     return :good
   rescue CanNotStrokeError => e
     return :notsyllable
   end
+
+  def inspect
+    @parts.join('')
+  end
+
+  def keys_down
+    @pressed.map do |hand|
+      hand.map.with_index {|pr, i| pr ? i : nil}.compact
+    end
+  end
 end
 
 def type_word(word)
+  all = []
   stroke = StrokeModel.new
-  count = 1
   word.each_char do |c|
     result = stroke.add(c)
     unless result == :good
-      count += 1
-      p stroke.parts
-      stroke.reset
+      all << stroke
+      stroke = StrokeModel.new
       stroke.add(c) # we need to actually use the failed letter
     end
   end
-  p stroke.parts
-  count
+  all << stroke
+  all
 end
 
-File.foreach(ARGV[0] || "/usr/share/dict/words") do |line|
+if (w = ARGV[1])
+  strokes = type_word(w)
+  p strokes
+  p strokes.map {|s| s.keys_down }
+  exit
+end
+
+sum = 0
+count = 0
+sc = 0
+len = 0
+f = IO.read(ARGV[0] || "/usr/share/dict/words").split
+f.each do |line|
   word = line.chomp.downcase
+  strokes = type_word(word)
+  next unless word =~ /^[a-z]+$/
+
+  count += 1
+  sum += strokes.length
+  sc += strokes.length/word.length.to_f
+  len += word.length
+
   if rand() < 0.0003 && word =~ /^[a-z]+$/
-    count = type_word(word)
-    puts "#{word}: #{count}"
+    puts "#{word}: #{strokes.count}"
+    p strokes
+    p strokes.map(&:keys_down)
   end
 end
+puts "Total words: #{count}"
+puts "Average word length: #{len/count.to_f}"
+puts "Average strokes/word: #{sum/count.to_f}"
+puts "Average characters per stroke: #{1.0/(sc/count.to_f)}"
